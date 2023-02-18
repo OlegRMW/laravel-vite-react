25ea61daa87b5782631a3442b9ec00a804f91bed
OlegRMW <zagudpev@mail.ru>
2023-02-18 23:36:49 +0300

    Initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import '../css/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ReactDOM from 'react-dom/client';
+import Home from './Page/Page';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Home />
+);
